2e1b24c375498b0df256ba0cfb8ed4ad6f5bf025
josh <josh-moss@live.nmit.ac.nz>
2020-03-10 19:19:46 +1300

    update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-let x = 100;
-let y = 55;
-let z = 10;
+/* using "Math.pow" command we can multiply the frist value with the second. 
+(,3 cubes the equasion, ,2 would square it). */ 
 
+let x = Math.pow (15, 3); 
+let y = Math.pow (5, 3);
+let z = Math.pow (55, 3);
 
-console.log(`${y + z} + ${x * z} =`, 65 + 1000);
-// (55 + 10 = 65) + (100 * 10 = 1000) = 65 + 1000 = 1065
+console.log(`${y + z} + ${x + z} =`, 166500 + 169750);
+//(5^3 + 55^3) + (100^3 + 55^3) , 166500 + 169750 = 336250
 
